698cad59c4e9ae530aac2579bfb62cdbf004a03c
YoruNoHikage <yorunohikage@gmail.com>
2013-05-14 18:39:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Déplacement-Robot
+=================
+
+Project I have made by myself during my studies. Be careful, this project is programmed in french.
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "appart.h"
+
+int main()
+{
+    printf("%s", appart);
+
+    return EXIT_SUCCESS;
+}
